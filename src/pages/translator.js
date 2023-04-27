3fa5b69b30ad448dc19da1957582c62749a76df6
Parth Barse <parthbarse72@gmail.com>
2023-04-28 01:11:13 +0530

    added bootstrap

--- src/pages/translator.js
+++ src/pages/translator.js
@@ -1,5 +1,7 @@
-export default function Translator(){
+export default function Translator() {
     return (
-        <h2>this is our Translator page</h2>
+        <div className="container">
+            <h2>this is our Translator page</h2>
+        </div>
     )
 }
